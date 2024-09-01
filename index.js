14e2168d647768279f6f6c513ba416d38e69df8e
siddiquimaha <siddiqui.mahajabeen@gmail.com>
2024-08-31 20:31:32 -0700

    it is in bootstrap

---
+++ index.js
@@ -0,0 +1,8 @@
+const prev = document.getElementById('prevLink');
+const next = document.getElementById('nextLink');
+// console.log(next);
+// console.log(prev);
+
+prev.addEventListener("click", () => {
+    console.log("you click on previous")
+})
